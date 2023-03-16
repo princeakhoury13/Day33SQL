ef2f3cae2e2391b79af0ba829f0828e7328af596
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-16 10:55:08 +0530

    added: new table added

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,1 +1,14 @@
-create database addressBook
+
+
+CREATE TABLE AddressBook (
+  id INT identity PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  state VARCHAR(50) NOT NULL,
+  zip VARCHAR(10) NOT NULL,
+  phone VARCHAR(20) NOT NULL,
+  email VARCHAR(100) NOT NULL
+)
+
