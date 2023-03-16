9521805d55eef82e9c691cb6cc9cd09523ad10f2
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-16 10:57:49 +0530

    added: new contact

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,14 +1,3 @@
+INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email) VALUES ('Tony', 'Stark', '123 Main St', 'NYC', 'NY', '12345', '555-555-5555', 'stark@email.com');
 
-
-CREATE TABLE AddressBook (
-  id INT identity PRIMARY KEY,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  address VARCHAR(100) NOT NULL,
-  city VARCHAR(50) NOT NULL,
-  state VARCHAR(50) NOT NULL,
-  zip VARCHAR(10) NOT NULL,
-  phone VARCHAR(20) NOT NULL,
-  email VARCHAR(100) NOT NULL
-)
-
+select * from AddressBook
