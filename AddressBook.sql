b6a223b5dedab58e1129b5383c32e30798b521a6
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-16 11:20:43 +0530

    added: count by contact type

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,10 +1,9 @@
-INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email) VALUES ('Tony', 'Stark', '123 Main St', 'NYC', 'NY', '12345', '555-555-5555', 'tony@email.com')
-INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email) VALUES ('Steve', 'Rogers', '123 Main St', 'EastBay', 'CA', '12345', '555-555-5555', 'rogers@email.com');
-INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email) VALUES ('Natasha', 'Romanoff', '123 Main St', 'EastBay', 'CA', '12345', '555-555-5555', 'rogers@email.com');
-INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email) VALUES ('Bucky', 'Barnes', '123 Main St', 'EastBay', 'CA', '12345', '555-555-5555', 'rogers@email.com');
+INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email, name, contact_type) VALUES ('Tony', 'Stark', '123 Main St', 'NYC', 'NY', '12345', '555-555-5555', 'tony@email.com', 'A1', 'Family')
+INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email, name, contact_type) VALUES ('Steve', 'Rogers', '123 Main St', 'EastBay', 'CA', '12345', '555-555-5555', 'rogers@email.com', 'A2', 'Professional');
+INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email, name, contact_type) VALUES ('Natasha', 'Romanoff', '123 Main St', 'EastBay', 'CA', '12345', '555-555-5555', 'romanoff@email.com', 'A3', 'Friends');
+INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email, name, contact_type) VALUES ('Bucky', 'Barnes', '123 Main St', 'EastBay', 'CA', '12345', '555-555-5555', 'buck@email.com', 'A3', 'Friends');
 
 select * from AddressBook
+ 
 
-ALTER TABLE AddressBook 
-  ADD name VARCHAR(100), 
-      contact_type VARCHAR(50) 
+SELECT contact_type, COUNT(*) AS count FROM AddressBook GROUP BY contact_type;
