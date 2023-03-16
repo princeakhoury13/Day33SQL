d349593b63c7cb55dc199f2cc9c5feac851e3309
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-16 11:07:34 +0530

    added: get count by city

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,6 +1,8 @@
 INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email) VALUES ('Tony', 'Stark', '123 Main St', 'NYC', 'NY', '12345', '555-555-5555', 'tony@email.com')
 INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email) VALUES ('Steve', 'Rogers', '123 Main St', 'EastBay', 'CA', '12345', '555-555-5555', 'rogers@email.com');
+INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email) VALUES ('Natasha', 'Romanoff', '123 Main St', 'EastBay', 'CA', '12345', '555-555-5555', 'rogers@email.com');
+INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email) VALUES ('Bucky', 'Barnes', '123 Main St', 'EastBay', 'CA', '12345', '555-555-5555', 'rogers@email.com');
 
 select * from AddressBook
 
-SELECT first_name FROM AddressBook WHERE city = 'NYC'
+SELECT city, COUNT(*) AS count FROM AddressBook GROUP BY city
