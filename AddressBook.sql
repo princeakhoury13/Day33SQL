25d45a7fbf473491f252a7803022d7c4fd5b4336
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-16 11:05:02 +0530

    added: retrieve person from given city

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,3 +1,6 @@
-DELETE FROM AddressBook WHERE first_name = 'Tony'
+INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email) VALUES ('Tony', 'Stark', '123 Main St', 'NYC', 'NY', '12345', '555-555-5555', 'tony@email.com')
+INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email) VALUES ('Steve', 'Rogers', '123 Main St', 'EastBay', 'CA', '12345', '555-555-5555', 'rogers@email.com');
 
 select * from AddressBook
+
+SELECT first_name FROM AddressBook WHERE city = 'NYC'
